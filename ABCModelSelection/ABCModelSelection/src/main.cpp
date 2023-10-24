7960f737cc6f3847d58fbc0472343fade178d81b
Carl Whitfield <carl.whitfield@manchester.ac.uk>
2023-10-24 14:30:43 +0100

    Removed MPI check from main
    Can just run directly with MPI compiler

--- ABCModelSelection/ABCModelSelection/src/main.cpp
+++ ABCModelSelection/ABCModelSelection/src/main.cpp
@@ -28,102 +28,103 @@
 
 std::shared_ptr<boost::random::mt19937> rng;  //pseudorandom number generator
 
-#if IS_WINDOWS
-	inline static HANDLE start_mpicode(const std::vector<std::string> & args)
-	{
-		std::stringstream ss;
-		int argc = int(args.size());
-		ss << args[0];
-		for(int i = 1; i < argc; i++)
-		{
-			ss << " " << args[i];
-		}
-		std::string cmd = ss.str().c_str();
-		PROCESS_INFORMATION pi = {0};
-		STARTUPINFO si = {0};
-		si.cb = sizeof(STARTUPINFO);
-		size_t size = cmd.size();
-		char* orig = new char [size+1];
-		copy(cmd.begin(), cmd.end(), orig);
-		orig[size] = '\0';
-		std::cout << cmd << std::endl;
-		wchar_t* input = new wchar_t [size+1];
-		size_t convertedChars = 0;
-		mbstowcs_s(&convertedChars, input, size+1, orig, _TRUNCATE);
-		delete [] orig;
-		if (CreateProcess(NULL, input, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi))
-		{
-			delete [] input;
-			CloseHandle(pi.hThread);
-			return pi.hProcess;
-		}
-		else
-		{
-			delete [] input;
-			return NULL;
-		}
-	}
-#else
-	inline static pid_t start_mpicode(const std::vector<std::string> &args)
-	{
-		std::stringstream ss;
-		int argc = int(args.size());
-		ss << args[0];
-		for (int i = 1; i < argc; i++)
-		{
-			ss << " " << args[i];
-		}
-		std::string cmd = ss.str();
+// #if IS_WINDOWS
+// 	inline static HANDLE start_mpicode(const std::vector<std::string> & args)
+// 	{
+// 		std::stringstream ss;
+// 		int argc = int(args.size());
+// 		ss << args[0];
+// 		for(int i = 1; i < argc; i++)
+// 		{
+// 			ss << " " << args[i];
+// 		}
+// 		std::string cmd = ss.str().c_str();
+// 		PROCESS_INFORMATION pi = {0};
+// 		STARTUPINFO si = {0};
+// 		si.cb = sizeof(STARTUPINFO);
+// 		size_t size = cmd.size();
+// 		char* orig = new char [size+1];
+// 		copy(cmd.begin(), cmd.end(), orig);
+// 		orig[size] = '\0';
+// 		std::cout << cmd << std::endl;
+// 		wchar_t* input = new wchar_t [size+1];
+// 		size_t convertedChars = 0;
+// 		mbstowcs_s(&convertedChars, input, size+1, orig, _TRUNCATE);
+// 		delete [] orig;
+// 		if (CreateProcess(NULL, input, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi))
+// 		{
+// 			delete [] input;
+// 			CloseHandle(pi.hThread);
+// 			return pi.hProcess;
+// 		}
+// 		else
+// 		{
+// 			delete [] input;
+// 			return NULL;
+// 		}
+// 	}
+// #else
+// 	inline static pid_t start_mpicode(const std::vector<std::string> &args)
+// 	{
+// 		std::stringstream ss;
+// 		int argc = int(args.size());
+// 		ss << args[0];
+// 		for (int i = 1; i < argc; i++)
+// 		{
+// 			ss << " " << args[i];
+// 		}
+// 		std::string cmd = ss.str();
 
-		pid_t childPID = fork();
-		if (childPID == 0)
-		{
-			// Child process
-			std::vector<char*> argv(argc + 1, nullptr);
-			for (int i = 0; i < argc; i++)
-			{
-				argv[i] = const_cast<char*>(args[i].c_str());
-			}
+// 		pid_t childPID = fork();
+// 		std::cout << "Child PID: " << childPID << std::endl;
+// 		if (childPID == 0)
+// 		{
+// 			// Child process
+// 			std::vector<char*> argv(argc + 1, nullptr);
+// 			for (int i = 0; i < argc; i++)
+// 			{
+// 				argv[i] = const_cast<char*>(args[i].c_str());
+// 			}
 
-			// Execute the command
-			execvp(argv[0], argv.data());
+// 			// Execute the command
+// 			execvp(argv[0], argv.data());
 
-			// This code is executed only if execvp fails
-			perror("execvp");
-			std::cout << "evecvp failed." << std::endl;
-			return -1;
-		}
-		else if (childPID > 0)
-		{
-			// Parent process
-			int status;
-			waitpid(childPID, &status, 0);
-			if (WIFEXITED(status))
-			{
-				int exitCode = WEXITSTATUS(status);
-				if (exitCode == 0)
-				{
-					std::cout << "Child process exited successfully." << std::endl;
-				}
-				else
-				{
-					std::cout << "Child process exited with error code: " << exitCode << std::endl;
-				}
-			}
-			else
-			{
-				std::cout << "Child process exited abnormally." << std::endl;
-			}
-		}
-		else
-		{
-			// Fork failed
-			perror("fork");
-		}
+// 			// This code is executed only if execvp fails
+// 			perror("execvp");
+// 			std::cout << "evecvp failed." << std::endl;
+// 			return -1;
+// 		}
+// 		else if (childPID > 0)
+// 		{
+// 			// Parent process
+// 			int status;
+// 			waitpid(childPID, &status, 0);
+// 			if (WIFEXITED(status))
+// 			{
+// 				int exitCode = WEXITSTATUS(status);
+// 				if (exitCode == 0)
+// 				{
+// 					std::cout << "Child process exited successfully." << std::endl;
+// 				}
+// 				else
+// 				{
+// 					std::cout << "Child process exited with error code: " << exitCode << std::endl;
+// 				}
+// 			}
+// 			else
+// 			{
+// 				std::cout << "Child process exited abnormally." << std::endl;
+// 			}
+// 		}
+// 		else
+// 		{
+// 			// Fork failed
+// 			perror("fork");
+// 		}
 
-		return childPID;
-	}
-#endif
+// 		return childPID;
+// 	}
+// #endif
 
 void assign_model_gens(MBWModelInputs *inputs,
 	                   std::vector<std::shared_ptr<ModelGenBaseClassName>> & ModelGens)
@@ -156,64 +157,64 @@ int main(int argc, char *argv[])
 	MPI_Comm_rank(MPI_COMM_WORLD, &i_core);
 	MPI_Comm_size(MPI_COMM_WORLD, &n_cores);
 	
-	//check MPI has been used, if not, use it
-	if(n_cores != N_PROCS)
-	{
-		//restart
-		std::vector<std::string> args(3 + argc);
-		args[0]="mpiexec";// -debug";
-		args[1]="-n";
-		std::stringstream ss;
-		ss << N_PROCS;
-		args[2]=ss.str().c_str();
+// 	//check MPI has been used, if not, use it
+// 	if(n_cores != N_PROCS)
+// 	{
+// 		//restart
+// 		std::vector<std::string> args(3 + argc);
+// 		args[0]="mpirun";// -debug";
+// 		args[1]="-n";
+// 		std::stringstream ss;
+// 		ss << N_PROCS;
+// 		args[2]=ss.str().c_str();
 
-		for (int i=0;i<argc;i++) 
-		{
-			ss.clear();
-			ss.str("");
-			ss << "\"" << argv[i] << "\"";
-			args[3+i]=ss.str().c_str();
-		}
+// 		for (int i=0;i<argc;i++) 
+// 		{
+// 			ss.clear();
+// 			ss.str("");
+// 			ss << "\"" << argv[i] << "\"";
+// 			args[3+i]=ss.str().c_str();
+// 		}
 
-#if IS_WINDOWS
-		//function to set mpi executable running in separate process (windows only)
-		HANDLE ff = start_mpicode(args);
-		if(ff != NULL)    //do not exit this code until child process is complete
-		{
-			// wait with ten-second checks
-			while (WAIT_TIMEOUT == WaitForSingleObject(ff, 10000))
-			{
-				// your wait code goes here.
-			}
+// #if IS_WINDOWS
+// 		//function to set mpi executable running in separate process (windows only)
+// 		HANDLE ff = start_mpicode(args);
+// 		if(ff != NULL)    //do not exit this code until child process is complete
+// 		{
+// 			// wait with ten-second checks
+// 			while (WAIT_TIMEOUT == WaitForSingleObject(ff, 10000))
+// 			{
+// 				// your wait code goes here.
+// 			}
 
-			// close the handle no matter what else.
-			CloseHandle(ff);
-			return 0;
-		}
-		else
-		{
-			std::cerr<<"warning: mpiexec failed"<<std::endl;
-			return 1;
-		}
-#else
-		pid_t ff = start_mpicode(args);
-		int status;
-		pid_t result = waitpid(ff, &status, WNOHANG);
-			// wait with ten-second checks
-		if (result != 0) {
-			std::cerr<<"warning: mpiexec failed"<<std::endl;
-			return -1;
-		}
-		else {
-			while (result == 0) {
-				// Child process is still running
-				// Your wait code goes here
-				sleep(10); // Example: Wait for 10 seconds
-			}
-			return 0;
-		}
-#endif
-	}
+// 			// close the handle no matter what else.
+// 			CloseHandle(ff);
+// 			return 0;
+// 		}
+// 		else
+// 		{
+// 			std::cerr<<"warning: mpiexec failed"<<std::endl;
+// 			return 1;
+// 		}
+// #else
+// 		pid_t ff = start_mpicode(args);
+// 		int status;
+// 		pid_t result = waitpid(ff, &status, WNOHANG);
+// 			// wait with ten-second checks
+// 		if (result != 0) {
+// 			std::cerr<<"warning: mpiexec failed"<<std::endl;
+// 			return -1;
+// 		}
+// 		else {
+// 			while (result == 0) {
+// 				// Child process is still running
+// 				// Your wait code goes here
+// 				sleep(10); // Example: Wait for 10 seconds
+// 			}
+// 			return 0;
+// 		}
+// #endif
+// }
 	//setup rng (different on each core)
 	unsigned int seed = static_cast<unsigned int>
 			   (std::chrono::high_resolution_clock::now().time_since_epoch().count());
